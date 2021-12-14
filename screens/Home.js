d92d193fdd539c05b22768b2f84a4d30e39c28ea
olorunyomi97 <olorunyomi97@gmail.com>
2021-12-14 17:10:13 +0100

    Native Scroll Animation

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, SafeAreaView, FlatList, TouchableWithoutFeedback, Image, ImageBackground, Animated,ScrollView, Touchable } from 'react-native';
 import { dummyData, COLORS, SIZES, FONTS, icons, images } from '../constants';
+import { Profiles } from '../components';
 
 
 const Home = ({ navigation }) => {
 
+    const newSeasonScrollX =  React.useRef(new Animated.Value(0)).current
+
+    // FUNCTION FOR RENDERING THE HEADER // 
     function renderHeader() {
         return (
             <View
@@ -60,6 +64,175 @@ const Home = ({ navigation }) => {
         )
     }
 
+     // FUNCTION FOR RENDERING THE BODY // 
+     function renderNewSeasonSection() {
+        return (
+            <Animated.FlatList 
+                horizontal
+                pagingEnabled
+                snapToAlignment="center"
+                snapToInterval={SIZES.width}
+                showsHorizontalScrollIndicator={false}
+                scrollEventThrottle={16}
+                decelerationRate={0}
+                contentContainerStyle={{
+                    marginTop: SIZES.radius
+                }}
+                data={dummyData.newSeason}
+                keyExtractor={item => `$item.id`}
+                onScroll={Animated.event([
+                    {nativeEvent: {contentOffset: {x:newSeasonScrollX}}}
+                ], {useNativeDriver: false})}
+                renderItem={({item, index}) => {
+                    return (
+                        <TouchableWithoutFeedback
+                            onPress={() => navigation.navigate('MovieDetail', { selectedMovie: item })}
+                        >
+                            <View
+                                style={{
+                                    width: SIZES.width,
+                                    alignItems: 'center',
+                                    justifyContent: 'center'
+                                }}
+                            >
+                                {/* Thumbnail */}
+                                <ImageBackground
+                                    source={item.thumbnail}
+                                    resizeMode="cover"
+                                    style={{
+                                        width: SIZES.width * 0.85,
+                                        height: SIZES.width * 0.85,
+                                        justifyContent: 'flex-end'
+                                    }}
+                                    imageStyle={{
+                                        borderRadius : 30
+                                    }}
+                                >
+                                    <View
+                                        style={{
+                                            flexDirection: 'row',
+                                            height: 60,
+                                            width: '100%',
+                                            marginBottom: SIZES.radius,
+                                            paddingHorizontal:SIZES.radius,
+                                            // backgroundColor:COLORS.primary
+                                        }}
+                                    >
+                                        {/* PLAY NOW */}
+                                        <View
+                                            style={{
+                                                flex:1,
+                                                flexDirection:'row',
+                                                alignItems: 'center'
+                                            }}
+                                        >
+                                            <View
+                                                style={{
+                                                    alignItems: 'center',
+                                                    justifyContent: 'center',
+                                                    width: 40,
+                                                    height: 40,
+                                                    borderRadius: 20,
+                                                    backgroundColor: COLORS.transparentWhite
+                                                }}
+                                            >
+                                                <Image
+                                                    source={icons.play}
+                                                    resizeMode='contain'
+                                                    style={{
+                                                        width: 15,
+                                                        height: 15,
+                                                        tintColor: COLORS.white
+                                                    }}
+                                                />
+                                            </View>
+                                                <Text
+                                                    style={{
+                                                        marginLeft: SIZES.base,
+                                                        color: COLORS.white,
+                                                        ...FONTS.h3
+                                                    }}
+                                                >
+                                                    Play Now
+                                                </Text>
+                                        </View>
+
+                                        {/* STILL WATCHING */}
+                                        {
+                                            item.stillWatching.length > 0 &&
+                                            <View
+                                                style={{
+                                                    justifyContent: 'center'
+                                                }}
+                                            >
+                                                <Text style={{color: COLORS.white, ...FONTS.h4}}>Still Watching</Text>
+                                                <Profiles 
+                                                    profiles={item.stillWatching}
+                                                />
+                                            </View>
+                                        }
+                                    </View>
+                                </ImageBackground>
+
+                            </View>
+                        </TouchableWithoutFeedback>
+                    )
+                }}
+            />
+            
+        )
+    }
+
+    // FUNCTION FOR RENDERING THE ANIMATION DOTS
+    function renderDots() {
+        const dotPosition = Animated.divide(newSeasonScrollX, SIZES.width)
+        return (
+            <View
+                style={{marginTop: SIZES.padding,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center'
+                }}
+            >
+                {dummyData.newSeason.map((item, index) => {
+
+                    const opacity = dotPosition.interpolate({
+                        inputRange: [index - 1, index, index + 1],
+                        outputRange: [0.3, 1, 0.3],
+                        extrapolate: 'clamp'
+                    })
+
+                    const dotWidth = dotPosition.interpolate({
+                        inputRange: [index - 1, index, index + 1],
+                        outputRange: [6, 20, 6],
+                        extrapolate: 'clamp'
+                    })
+                    const dotColor = dotPosition.interpolate({
+                        inputRange: [index - 1, index, index + 1],
+                        outputRange: [COLORS.lightGray, COLORS.primary, COLORS.lightGray],
+                        extrapolate: 'clamp'
+                    })
+
+                    return (
+                        <Animated.View
+                            key={`dot-${index}`}
+                            opacity={opacity}
+                            style={{
+                                borderRadius: SIZES.radius,
+                                marginHorizontal: 3,
+                                width: 6,
+                                height: 6,
+                                backgroundColor: COLORS.primary
+                            }}
+                        />
+                    )
+                })
+    
+                }
+            </View>
+        )
+    }
+
     return (
         <SafeAreaView
             style={{
@@ -68,6 +241,18 @@ const Home = ({ navigation }) => {
             }}
         >
             {renderHeader()}
+
+            <ScrollView
+                contentContainerStyle={{
+                    paddingBottom: 100
+                }}
+            >
+                {renderNewSeasonSection()}
+
+                {renderDots()}
+            </ScrollView>
+
+
         </SafeAreaView>
     )
 }
